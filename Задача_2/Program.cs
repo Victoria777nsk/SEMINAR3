62b3359c706eea6538d15043478e73f0191da49b
Victoria <victoriaerygina@yandex.ru>
2022-08-26 13:47:34 +0700

    Определение диапазона координат по номеру четверти плоскости

---
+++ Задача_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine ("Введите номер четверти: ");
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+if (quarterNumber == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (quarterNumber == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if (quarterNumber == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if (quarterNumber == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
