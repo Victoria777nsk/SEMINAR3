6136b3287bae5ee14b9db5a5e41902c6b51e4a11
Victoria <victoriaerygina@yandex.ru>
2022-08-26 13:18:01 +0700

    Определение четверти плоскости по координатам двух точек

---
+++ Задача_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0, 
+// и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите X: ");
+        int X = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите Y: ");
+        int Y = Convert.ToInt32(Console.ReadLine());
+
+        if (X > 0 && Y > 0)
+        {
+            Console.WriteLine("1 четверть");
+        }
+        else if (X < 0 && Y > 0)
+        {
+            Console.WriteLine("2 четверть");
+        }
+        else if (X < 0 && Y < 0)
+        {
+            Console.WriteLine("3 четверть");
+        }
+        else if (X > 0 && Y < 0)
+        {
+            Console.WriteLine("4 четверть");
+        }
+        else 
+        {
+            Console.WriteLine("Ошибка! X и Y НЕ могут быть равны 0");
+        }
+    }
+}
