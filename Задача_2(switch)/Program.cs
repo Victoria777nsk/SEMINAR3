8950ed269a0d5edc7ad6e90991239403ae29a12e
Victoria <victoriaerygina@yandex.ru>
2022-08-26 13:54:07 +0700

    Определение диапазона координат с помощью switch

---
+++ Задача_2(switch)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+switch (quarter)
+{
+    case 1: Console.WriteLine("X > 0, Y > 0"); break;
+    case 2: Console.WriteLine("X < 0, Y > 0"); break;
+    case 3: Console.WriteLine("X < 0, Y < 0"); break;
+    case 4: Console.WriteLine("X > 0, Y < 0"); break;
+    default: Console.WriteLine("Введена неправильная четверть"); break;
+}
